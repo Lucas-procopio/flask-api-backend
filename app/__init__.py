56f448f765ddb5b7c0a91ee7fa026258258f9a23
lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-10-03 22:42:40 -0300

    add serealizer.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_migrate import Migrate
-from .model import configure
+from .model import configure as config_db
+from .serealizer import configure as config_ma
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/FLASK-API-BACKEND.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    configure(app) # passing config informations of database to app in Flask
+    config_db(app) # passing config informations of database to app in Flask
+    config_ma(app)
 
     Migrate(app, app.db)
     return app
